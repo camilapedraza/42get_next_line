c8d690a2831abfe9d86737a2d86e06ce7873d307
Maria Camila Pedraza Casasbuenas <mpedraza@f1r12s13.clusters.42paris.fr>
2025-11-24 23:03:01 +0100

    protocode v3 - smaller functions

---
+++ old/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpedraza <mpedraza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/20 13:58:51 by mpedraza          #+#    #+#             */
+/*   Updated: 2025/11/24 22:15:43 by mpedraza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <stdint.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10000000
+#  endif
+
+# define STASH_SIZE 20000
+
+char	*get_next_line(int fd);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+
+# endif
